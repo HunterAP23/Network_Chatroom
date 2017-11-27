ed2499097f01efe6ccda1eda65745311574b5776
Zephyrum-Alsend <isfaryl@gmail.com>
2017-11-27 06:05:03 -0500

    ServerClass.cpp ServerClass.h
    I fixed my code and added it to the files already containing some work as functions.  Login functionality is not implemented as I still need to peruse all the pre-existing code and I need confirmation on what the server should be expecting from the client.

--- ServerClass.h
+++ ServerClass.h
@@ -1,10 +1,20 @@
 #pragma once
-#include "stdafx.h"
+#include <string>
+#include <WinSock2.h>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
 
 struct client;
 
 //maximum clients we will serve
 const int MAX_CLIENTS = 64;
+//maximum line size to be read/sent at once
+const int LINESZ = 2048;
+//maximum number of clients allowed to be waiting for a connection
+const int QUEUESZ = 10;
 
 //class ServerClass
 //Handles all server functions, including password checking, etc.
@@ -13,13 +23,27 @@ class ServerClass
 public:
 
 	//ServerClass();
+	//requests service and listening port
+	void InitServer();
+
+	//main server routine
+	bool receiveMsg(SOCKET ls);
+
+	//sends an error message to all clients if the server experienced a terminal error.
+	void serverFailure(std::vector<SOCKET> c) {
+		char buff[LINESZ] = "500: Internal Server Error";
+		sendAll(c, &buff[0], strlen(buff));
+	}
+
+	//sends a message, str, to all connected users.
+	bool sendAll(std::vector<SOCKET> c, const char * str, int nb);
 
 
-
+	bool LoginAttempt(client& x);
 	//getter setter for whitelistpath
 	std::string GetWhiteList();
 	void SetWhiteList(std::string NewPath);
-	
+
 	//for map version, builds whitelist from text file at ?server start? or have a function to UpdateWhiteList mid server
 	void LoadWhiteList();
 
@@ -27,21 +51,21 @@ public:
 	bool NewConnect(SOCKET s);
 	//part of handshake process, checks if client has updated security code
 	bool CheckSecurityCode(int code);
-		//checks WhiteList for username + password combination returns true if found false if not
+	//checks WhiteList for username + password combination returns true if found false if not
 	bool CheckWhiteList(std::string UserName, std::string PassWord);
 
 protected:
-	
 
 
-private: 
+
+private:
 	std::map<std::string, std::string> WhiteListMap;
 	// path for where the WhiteList (assortment of usernames and passwords
-	std::string m_WhiteList="WhiteList.txt"; //default val
+	std::string m_WhiteList = "WhiteList.txt"; //default val
 	int keycode = 1312; //default val
 	SOCKET ListeningSocket; //store our listening socket
-	//connected client array
-	//SOCKET SOC[MAX_CLIENTS]; 
+							//connected client array
+							//SOCKET SOC[MAX_CLIENTS]; 
 	std::vector<client> SOC;
 };
 
@@ -50,7 +74,7 @@ struct client
 public:
 	SOCKET s; //socket this client is on, identifying feature I guess? 
 	enum ClientStatus { Code, PassWord, LoggedIn }; //login status, code means they need to supply the code, password for loggin in process
-	//logged in means they have fully logged in and are participating in the chat and recieving messages
+													//logged in means they have fully logged in and are participating in the chat and recieving messages
 
 protected:
 
